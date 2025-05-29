907535d54d7b0240ab416660aa27155aed388f9c
TanishB567 <bansaltanish05@gmail.com>
2025-05-29 15:08:39 +0100

    fix: another one

--- main/views.py
+++ main/views.py
@@ -7,5 +7,5 @@ def simple_json_view(request):
     # if not UserProfile.objects.exists():
     #     UserProfile.objects.create(name="Test User", email="test@example.com")
 
-    data = list(UserProfile.objects.values('name', 'email'))
+    # data = list(UserProfile.objects.values('name', 'email'))
     return JsonResponse({'question': '5x = 0. What is x = ?'}, safe=False)
